fd6c329a9f217f02eb397772ac6563fd1bee5992
leonaiyem <mdnaiyem.islam.10@gmail.com>
2025-08-01 20:32:05 +0600

    preloader added

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-
-// https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
 export default defineConfig({
   plugins: [react()],
-  // base: "/Projects/React/clinic-os/",
   server: {
     proxy: {
       "/api": {
